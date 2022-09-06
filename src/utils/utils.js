e351e538aa55433101ed2f27102bd59f4df5f286
chenhaolin <845734834@qq.com>
2022-09-06 09:52:41 +0800

    处理报错

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,8 @@
-import fetch from "node-fetch";
-
+const fetch = require('node-fetch')
 const requestFetch = function (url, options) {
 	return fetch(url, options).then(res => res.json())
 }
 
 
-exports.requestFetch = requestFetch;
+// exports.requestFetch = requestFetch;
+module.exports = requestFetch
